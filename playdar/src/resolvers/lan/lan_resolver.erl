3f0e1af0848ee5a16626631cf34080f27d7e923e
Richard Jones <rj@metabrew.com>
2009-10-01 14:48:36 +0100

    make lan resolver drop dupes

--- playdar/src/resolvers/lan/lan_resolver.erl
+++ playdar/src/resolvers/lan/lan_resolver.erl
@@ -1,3 +1,6 @@
+%% Uses UDP multicast to resolve against other instances of this code
+%% running elsewhere on the LAN. Streams via HTTP.
+
 -module(lan_resolver).
 
 -behaviour(gen_server).
@@ -10,7 +13,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
      terminate/2, code_change/3]).
 
--record(state, {sock}).
+-record(state, {sock, seenqids}).
 -define(BROADCAST, {239,255,0,1}).
 
 %% API
@@ -19,16 +22,20 @@ resolve(Q, Qpid)        -> gen_server:cast(?MODULE, {resolve, Q, Qpid}).
 weight()                -> 95.
 targettime()            -> 50.
 
+%% Used as a callback when qry results arrive:
 send_response(A, Qid, Ip, Port) -> 
     gen_server:cast(?MODULE, {send_response, A, Qid, Ip, Port}).
 
 %% gen_server callbacks
 init([]) ->
-    LAddr = {192,168,1,67},
+	{R1,R2,R3} = now(),
+	random:seed(R1,R2,R3),
+	SQ = ets:new(seenqids,[]),
+	LAddr = {0,0,0,0},
     {ok, Sock} = gen_udp:open(60210, [binary, 
                                      {reuseaddr, true},{ip, ?BROADCAST}, 
                                      {add_membership, {?BROADCAST, LAddr}}]),
-    {ok, #state{sock=Sock}}.
+    {ok, #state{sock=Sock, seenqids=SQ}}.
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
@@ -36,9 +43,17 @@ handle_call(_Request, _From, State) ->
 
 handle_cast({resolve, Q, Qpid}, State) ->
     {struct, Parts} = Q,
-    Msg = {struct, [{<<"_msgtype">>, <<"rq">>},{<<"qid">>,qry:qid(Qpid)}|Parts]},
-    gen_udp:send(State#state.sock, ?BROADCAST, 60210, mochijson2:encode(Msg)),
-    {noreply, State};
+	Qid = qry:qid(Qpid),
+	% Ignore if we've dealt with this qid already
+	case ets:lookup(State#state.seenqids, Qid) of
+		[{Qid, true}] -> 
+			{noreply, State};
+		_ ->
+			ets:insert(State#state.seenqids, {Qid,true}),
+			Msg = {struct, [ {<<"_msgtype">>, <<"rq">>},{<<"qid">>,Qid} | Parts ]},
+			gen_udp:send(State#state.sock, ?BROADCAST, 60210, mochijson2:encode(Msg)),
+			{noreply, State}
+	end;
  
 handle_cast({send_response, A, Qid, Ip, Port}, State) ->
     %io:format("lan sending response: ~w ~w ~w ~w~n", [A, Qid, Ip, Port]),
@@ -51,7 +66,7 @@ handle_cast({send_response, A, Qid, Ip, Port}, State) ->
     gen_udp:send(State#state.sock, Ip, Port, mochijson2:encode(Msg)),
     {noreply, State}.
 
-handle_info({udp, Socket, {A,B,C,D}=Ip, InPortNo, Packet}, State) when Ip /=  {192,168,1,67} ->
+handle_info({udp, _Socket, {A,B,C,D}=Ip, _InPortNo, Packet}, State) ->
     io:format("RCVD: ~p~n", [Packet]),
     {struct, L} = mochijson2:decode(Packet),
     case proplists:get_value(<<"_msgtype">>,L) of
@@ -64,23 +79,27 @@ handle_info({udp, Socket, {A,B,C,D}=Ip, InPortNo, Packet}, State) when Ip /=  {1
                     Url = io_lib:format("http://~w.~w.~w.~w:60210/sid/~s",
                                         [A,B,C,D,Sid]),
                     qry:add_result(Qpid, {struct, 
-                                          [{<<"url">>, list_to_binary(Url)}|L2]});
+                                          [{<<"url">>, list_to_binary(Url)}|L2]}),
+                    {noreply, State};
                 _ ->
-                    io:format("noop1",[])
+                    {noreply, State}
             end;
         
         <<"rq">> ->
             Qid = proplists:get_value(<<"qid">>, L),
-            Cbs = [ fun(Ans)-> ?MODULE:send_response(Ans, Qid, Ip, 60210) end ],
-            resolver:dispatch({struct,L}, Qid, Cbs);
+            % do nothing if we dispatched, or already received this qid
+            case ets:lookup(State#state.seenqids, Qid) of
+                [{Qid, true}] -> {noreply, State};
+                _ ->           
+                    ets:insert(State#state.seenqids, {Qid,true}),
+                    Cbs = [ fun(Ans)-> ?MODULE:send_response(Ans, Qid, Ip, 60210) end ],
+                    resolver:dispatch({struct,L}, Qid, Cbs),
+                    {noreply, State}
+            end;
             
-        _ -> io:format("noop2",[])
-    end,
-    {noreply, State};
+        _ -> {noreply, State}
+    end.
     
-handle_info({udp, _Socket, _Ip, _InPortNo, _Packet}, State) ->
-    {noreply, State}.
-
 terminate(_Reason, State) ->
     gen_udp:close(State#state.sock),
     ok.
