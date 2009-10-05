a46759e66775b2795175978ca4a39e693220a933
Richard Jones <rj@metabrew.com>
2009-10-05 15:51:54 +0100

    ensure random:uniform gets an int, else it fails on windows

--- playdar/src/utils.erl
+++ playdar/src/utils.erl
@@ -6,10 +6,10 @@
 uuid_gen() -> list_to_binary(string:to_upper(to_string(v4()))).
 
 v4() ->
-    v4( random:uniform(math:pow(2, 48)) - 1, 
-        random:uniform(math:pow(2, 12)) - 1, 
-        random:uniform(math:pow(2, 32)) - 1, 
-        random:uniform(math:pow(2, 30)) - 1).
+    v4( random:uniform(round(math:pow(2, 48))) - 1, 
+        random:uniform(round(math:pow(2, 12))) - 1, 
+        random:uniform(round(math:pow(2, 32))) - 1, 
+        random:uniform(round(math:pow(2, 30))) - 1).
 v4(R1, R2, R3, R4) ->
     <<R1:48, 4:4, R2:12, 2:2, R3:32, R4: 30>>.
 to_string(U) ->
