cc258831dd8f0f286985b3ab4cbaf490a4b8a3e3
Max Howell <max@methylblue.com>
2009-10-07 19:54:49 +0100

    Use taglib_config for taglib-driver cflags

--- priv/taglib_driver/build.sh
+++ priv/taglib_driver/build.sh
@@ -1,1 +1,1 @@
-g++ -I/usr/include/taglib/ -lboost_filesystem-mt -ltag -o ./taglib_json_reader taglib_json_reader.cpp
+g++ `taglib-config --cflags` -ltag -o ./taglib_json_reader taglib_json_reader.cpp
