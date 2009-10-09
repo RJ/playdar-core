a90ae1024e7fbfbedb219f3a62e62cbbb8761c18
Richard Jones <rj@metabrew.com>
2009-10-09 19:16:58 +0100

    temp fix for log macro

--- include/playdar.hrl
+++ include/playdar.hrl
@@ -1,17 +1,7 @@
 %TODO should probably have a log server that buffers and writes to a log file..
 
 % logging macros, eg: ?L(info, "something is: ~s", [S]).
--define(LOG(Log_Level, Log_Format, Log_Args), 
-        begin 
-            case Log_Level of
-                debug -> noop;
-                _ ->
-                    {{Log_Y,Log_M,Log_D},{Log_Hr,Log_Min,Log_Sec}} = erlang:localtime(),
-                    io:format("~w/~2..0w/~2..0w ~2..0w:~2..0w:~2..0w ~w ~w\t" ++ Log_Format ++ "\n",
-                              [Log_Y, Log_M, Log_D, Log_Hr,Log_Min,Log_Sec,?MODULE,Log_Level|Log_Args])
-            end,
-            ok
-        end ).
+-define(LOG(Log_Level, Log_Format, Log_Args), playdar:do_log(Log_Level, Log_Format, Log_Args)).
 
 
 -define(CONFVAL(ConfVal_K,ConfVal_Def), playdar_config:confval(ConfVal_K,ConfVal_Def)).
--- src/playdar.erl
+++ src/playdar.erl
@@ -5,7 +5,7 @@
 
 -module(playdar).
 -author('author <author@example.com>').
--export([start/0, stop/0]).
+-export([start/0, stop/0, do_log/3]).
 
 ensure_started(App) ->
     case application:start(App) of
@@ -28,3 +28,15 @@ stop() ->
     Res = application:stop(playdar),
     application:stop(crypto),
     Res.
+
+% TODO proper logging!
+% for now, this fun is called by the ?LOG macro
+do_log(Log_Level, Log_Format, Log_Args) ->  
+    case Log_Level of
+        debug -> noop;
+        _ ->
+            {{Log_Y,Log_M,Log_D},{Log_Hr,Log_Min,Log_Sec}} = erlang:localtime(),
+            io:format("~w/~2..0w/~2..0w ~2..0w:~2..0w:~2..0w ~w ~w\t" ++ Log_Format ++ "\n",
+                      [Log_Y, Log_M, Log_D, Log_Hr,Log_Min,Log_Sec,?MODULE,Log_Level|Log_Args])
+    end,
+    ok.
