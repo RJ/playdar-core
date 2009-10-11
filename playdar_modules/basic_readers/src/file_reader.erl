5f618858a66eded919d3f751b9e8eaafaa1c33ca
Richard Jones <rj@metabrew.com>
2009-10-11 19:41:49 +0100

    loging tweak

--- playdar_modules/basic_readers/src/file_reader.erl
+++ playdar_modules/basic_readers/src/file_reader.erl
@@ -15,9 +15,9 @@ reader_protocols() ->
 % starts sending file data to Pid
 run({struct, A}, Pid, Ref) ->
     "file://"++Path = binary_to_list(proplists:get_value(<<"url">>, A)),
+    ?LOG(info, "Requesting ~p", [Path]),
     case file:open(Path, [read, binary]) of
         {ok, Io} ->
-            ?LOG(info, "Serving file ~s", [Path]),
             Mimetype=proplists:get_value(<<"mimetype">>, A, <<"binary/unknown">>),
             {ok, FileInfo} = file:read_file_info(Path),
             Headers = [ {"content-type", binary_to_list(Mimetype)},
