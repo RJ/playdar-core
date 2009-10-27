8fb5a40e4ccab2ddc53a44bd8ee4e3bb75d8ed11
Lucas Hrabovsky <lucas@galacticus.(none)>
2009-10-27 17:06:27 -0400

    rewrote amie street resolver to use perplexes resolver_lib that cleaned things up substantially.

--- contrib/amiestreet/amiestreet-resolver.py
+++ contrib/amiestreet/amiestreet-resolver.py
@@ -14,85 +14,51 @@
 # http://bitbucket.org/rg3/youtube-dl/wiki/Home
  
 ########################################################################
-import sys
+import sys, os
 import simplejson as json
 from struct import unpack, pack
 from amieapi import AmieStreet
 
-##########################################
-# Amie Street Resolver
-##########################################
-def get_tracks(artist_name, track_name):
-    print >>sys.stderr, 'Amie Street: searching for', artist_name, track_name, '\r'
-    songs = []
-    amiestreet = AmieStreet('http://beta.amiestreet.com/api/v0.1/');
-    results = amiestreet.SearchApi_find(artist_name, track_name);
-    songs = results['songs']
-    
-    if songs[0]['found']==True:
-        print >>sys.stderr, 'Amire Street: FOUND IT', '\r'
-        songObject = amiestreet.SongApi_getSongById(songs[0]['id']);
-        song = {};
-        song['artist']  = songObject['artist']['name']
-        song['album']   = songObject['album']['title']
-        song['track']   = songObject['title']
-        song['source']  = 'AmieStreet.com'
-        song['score']   = 1.00;
-        song['url']     = 'http://amiestreet.com/stream/song'+str(songObject['id'])+'.mp3'
-        song['isSample']= not songObject['actorHasSong']
-        return [song];
-    
-    print >>sys.stderr, 'Amie Street: found nothing', '\r'
-    
-    return [];
+my_dir = os.path.dirname(os.path.realpath(__file__))
+os.chdir(my_dir)
 
-###################################################################### functions
-def print_json(o):
-    s = json.dumps(o)
-    sys.stdout.write(pack('!L', len(s)))
-    sys.stdout.write(s)
-    sys.stdout.flush()
- 
-def resolve(artist, track):
-    return get_tracks(artist, track)
- 
- 
-def test():
-    print get_tracks('People Under The Stairs', 'Beer');
+sys.path.insert(0, os.path.join(my_dir, '../resolver_libs/'))
 
-####################################################################### settings
-settings = dict()
-settings["_msgtype"] = "settings"
-settings["name"] = "Amie Street Resolver"
-settings["targettime"] = 800
-settings["weight"] = 50
-print_json( settings )
- 
-###################################################################### main loop
-while 1:
-    length = sys.stdin.read(4)
- 
-    if not length:
-        break;
- 
-    length = unpack('!L', length)[0]
-    if not length:
-        break
-    # something probably went wrong, most likely we're out of sync and are
-    # reading the 4 bytes length header in the middle of a json string. We can't
-    # recover. Bail.
-    if length > 4096 or length < 0:
-        break
-    if length > 0:
-        msg = sys.stdin.read(length)
+import playdar_resolver
+
+asClient = None
+
+def getASApiClient():
+    if asClient != None:
+        return asClient
+
+    return AmieStreet('http://beta.amiestreet.com/api/v0.1/')
+
+class AmieStreetResolver(playdar_resolver.PlaydarResolver):
+        def resolver_settings(self):
+            """My settings"""
+            return {'name':"Amie Street Resolver", 'targettime':800, 'weight':50}
+
+        def results(self, query):
+            results = getASApiClient().SearchApi_find(query['artist'], query['track']);
+            songs = results['songs']
+            
+            if songs[0]['found']==True:
+                songObject = getASApiClient().SongApi_getSongById(songs[0]['id']);
+                song = {};
+                song['artist']  = songObject['artist']['name']
+                song['album']   = songObject['album']['title']
+                song['track']   = songObject['title']
+                song['source']  = 'AmieStreet.com'
+                song['score']   = 1.00;
+                song['url']     = 'http://amiestreet.com/stream/song'+str(songObject['id'])+'.mp3'
+                song['isSample']= not songObject['actorHasSong']
+                return [song]
+            
+            return []
+                
+if __name__ == "__main__":
         try:
-            request = json.loads(msg)
-            # print request
-            tracks = resolve(request['artist'], request['track'])
-            if len(tracks) > 0:
-                response = { 'qid':request['qid'], 'results':tracks, '_msgtype':'results' }
-                print_json(response)
+                AmieStreetResolver.start_static()
         except:
-            # safe to continue, skipping this msg, because at least
-            # we consumed enough input so next iteration hits size header.
-            pass
+                traceback.print_exc(file=sys.stderr)
