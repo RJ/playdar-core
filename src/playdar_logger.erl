409183670bf4b9899d36dd3b7248a50085dad07b
RJ <rj@metabrew.com>
2009-11-01 17:57:41 +0000

    remove unneeded export

--- src/playdar_logger.erl
+++ src/playdar_logger.erl
@@ -3,7 +3,7 @@
 -behaviour(gen_server).
 
 -export([start_link/0, do_log/4, register_logger/3]).
--export([http_req/2,  slashes/1]).
+-export([http_req/2]).
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
